43d10a0b48998302324f2a9bb83631383c08e7c9
Diegoisawesome <diego@domoreaweso.me>
2016-07-27 00:12:24 -0700

    Diego's stuff

---
+++ Hololens Skype/Assets/Scripts/CardCommands.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardCommands : MonoBehaviour {
+
+    public GameObject alertCard;
+
+    void Start()
+    {
+        Renderer[] rendererObjects = alertCard.GetComponentsInChildren<Renderer>();
+
+        for (int i = 0; i < rendererObjects.Length; i++)
+        {
+            rendererObjects[i].enabled = false;
+        }
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        Renderer[] renderObjects = alertCard.GetComponentsInChildren<Renderer>();
+        
+        if (!renderObjects[0].enabled)
+            alertCard.GetComponent<FadeObjectInOut>().FadeIn();
+    }
+}
---
+++ Hololens Skype/Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SphereCommands : MonoBehaviour
+{
+    public GameObject cardCollection;
+
+    void Start()
+    {
+        Renderer[] rendererObjects = cardCollection.GetComponentsInChildren<Renderer>();
+
+        for (int i = 0; i < rendererObjects.Length; i++)
+        {
+            rendererObjects[i].enabled = false;
+        }
+    }
+    
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        Renderer[] renderObjects = cardCollection.GetComponentsInChildren<Renderer>();
+
+        if (!renderObjects[0].enabled)
+            cardCollection.GetComponent<FadeObjectInOut>().FadeIn();
+    }
+}
